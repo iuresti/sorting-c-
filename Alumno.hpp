99eacb6a35c64013e17672fae0fc557f3cf7f124
Iván Uresti <ivan.uresti@gmail.com>
2020-10-10 09:11:51 -0500

    Update Alumno.hpp
    Anotaciones

--- Alumno.hpp
+++ Alumno.hpp
@@ -26,6 +26,9 @@ public:
     ~Alumno();
 };
 
+// Te recomiendo separar la declaración (en un archivo .h) y la implementación (en un archivo .cpp) .. 
+// con los templates creo que no se puede, pero si la clase no usa templates, mejor sepárala
+
 Alumno::Alumno() {}
 
 Alumno::Alumno(string _firstName, string _lastName, int _clave /*,clock_t _birthday*/)
@@ -50,6 +53,13 @@ int Alumno::getclave()
     return clave;
 }
 
+// Utiliza referencias cuando pases objetos por parámetro, en C++ debes tener cuidado al pasar objetos por parámetro .. si no son referencias 
+// llamas al constructor de copia al entrar y al destructor al salir innecesariamente, generando más memoria y procesamiento... incluso hay casos donde
+// podría generar un bug
+//
+// Es decir, tu parámetro debería ser: string &_firstName
+// O mejor aún.. const string &_firstName
+// Una referencia constante da la certeza de que el objeto pasado por parámetro no será modificado (el estado) dentro del método que lo usa
 void Alumno::setfirstName(string _firstName)
 {
     firstName = _firstName;
