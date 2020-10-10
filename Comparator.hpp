98e24c769a8ec4fa29d141f782dc5a81649d0997
Iván Uresti <ivan.uresti@gmail.com>
2020-10-10 09:14:15 -0500

    Update Comparator.hpp
    Cambiar a referencia constante

--- Comparator.hpp
+++ Comparator.hpp
@@ -2,5 +2,6 @@ class Comparator
 {
     public:
     template <typename T>
-    int compare(T, T);
+    // Como no sabes qué vas a recibir y sabes que no vas a modificar los datos que recibas, deberías especificar que son refencias constantes
+    int compare(const T &, const T &);
 };
